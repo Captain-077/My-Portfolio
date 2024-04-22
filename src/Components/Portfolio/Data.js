a56be096cb1659b7cf9dbd7dfac26b082e1157b8
Your Name <nakuldesigner77@gmail.com>
2024-04-22 19:27:53 +0530

    shopping cart removed

--- src/Components/Portfolio/Data.js
+++ src/Components/Portfolio/Data.js
@@ -18,16 +18,16 @@ export const ProjectsData = [
         techstack: "#HTML, #CSS, #JS, #Rest-APIs, #React-Router, #React",
         github: "https://github.com/Captain-077/My-Crud-App"
     },
-    {
-        id: 2,
-        category: "React Projects",
-        title: 'Shopping Cart App (Pending....)',
-        image: shoppingCart,
-        description: "A simple Shopping cart App",
-        link: 'https://nakul-ecommerce-app.vercel.app/',
-        techstack: "#HTML, #CSS, #JS, #Rest-APIs, #React-Router, #React, #Redux",
-        github: "https://github.com/Captain-077/Shopping-Cart-App"
-    },
+    // {
+    //     id: 2,
+    //     category: "React Projects",
+    //     title: 'Shopping Cart App (Pending....)',
+    //     image: shoppingCart,
+    //     description: "A simple Shopping cart App",
+    //     link: 'https://nakul-ecommerce-app.vercel.app/',
+    //     techstack: "#HTML, #CSS, #JS, #Rest-APIs, #React-Router, #React, #Redux",
+    //     github: "https://github.com/Captain-077/Shopping-Cart-App"
+    // },
 
     {
         id: 3,
